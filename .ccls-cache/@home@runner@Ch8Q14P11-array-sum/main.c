abeb66e8813c7dba39ae28f1a5d061b588c8bee7
Iain Taylor <iain.taylor.94@gmail.com>
2023-08-05 14:49:06 +0000

    forked repl

---
+++ .ccls-cache/@home@runner@Ch8Q14P11-array-sum/main.c
@@ -0,0 +1,36 @@
+// Program to calculate the sum of the elements of an array
+
+#include <stdio.h>
+
+// Declare Functions
+int arraySum (int, int[]);
+
+int main(void) {
+  // Get input from terminal
+  int numElements;
+  printf ("Enter the number of elements: ");
+  scanf ("%d", &numElements);
+  int inputArray[numElements];
+  printf ("Enter the elements of the array: ");
+  for (int i = 0; i < numElements; i++)
+    scanf ("%d", &inputArray[i]);
+
+  // Calculate sum of array
+  int sumArray = arraySum (numElements, &inputArray[0]);
+
+  // Print result
+  printf ("The sum of the array is: %d\n", sumArray);
+  
+  return 0;
+}
+
+// Define Functions
+int arraySum (int numElements, int array[]) {
+  
+  int sum = 0;
+  for (int i = 0; i < numElements; i++) {
+    sum += array[i];
+  }
+
+  return (sum);
+}
